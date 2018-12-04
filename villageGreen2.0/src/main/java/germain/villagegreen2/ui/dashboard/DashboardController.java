bf6a86f11717402555d4ae3b4fac78a009714b57
Germain SIPIERE <gsipiere@gmail.com>
2018-12-04 11:37:16 +0100

    add empty dashboard

--- villageGreen2.0/src/main/java/germain/villagegreen2/ui/dashboard/DashboardController.java
+++ villageGreen2.0/src/main/java/germain/villagegreen2/ui/dashboard/DashboardController.java
@@ -10,13 +10,25 @@
  */
 package germain.villagegreen2.ui.dashboard;
 
+import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.chart.PieChart;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TreeTableColumn;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import org.kordamp.ikonli.javafx.FontIcon;
 
 /**
  * FXML Controller class
@@ -30,7 +42,27 @@ public class DashboardController implements Initializable {
     @FXML
     private ImageView logo;
     @FXML
-    private PieChart typeChart;
+    private Tab ClientsTab;
+    @FXML
+    private TreeTableColumn<?, ?> nomCol;
+    @FXML
+    private TreeTableColumn<?, ?> prenomCol;
+    @FXML
+    private TreeTableColumn<?, ?> telcol;
+    @FXML
+    private TreeTableColumn<?, ?> mailCol;
+    @FXML
+    private TreeTableColumn<?, ?> typeCol;
+    @FXML
+    private Tab statTab;
+    @FXML
+    private FontIcon detailButton;
+    @FXML
+    private JFXButton ajoutButton;
+    @FXML
+    private JFXButton deletButton;
+    @FXML
+    private JFXButton closeButton;
 
     /**
      * Initializes the controller class.
@@ -38,6 +70,30 @@ public class DashboardController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    @FXML
+    private void addHandler(ActionEvent event) {
+        loadWindow("/fxml/ClientAdd.fxml","Ajout Client");
+        System.out.println("bouton fonctionnel");
+    }
+
+    @FXML
+    private void closeHandler(ActionEvent event) {
+        System.out.println("bouton fonctionnel");
+
+        System.exit(0);
+    }
+
+    void loadWindow(String loc, String title) {
+        try {
+            Parent parent = FXMLLoader.load(getClass().getResource(loc));
+            Stage stage = new Stage(StageStyle.DECORATED);
+            stage.setTitle(title);
+            stage.setScene(new Scene(parent));
+            stage.show();
+        } catch (IOException ex) {
+            Logger.getLogger(DashboardController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
